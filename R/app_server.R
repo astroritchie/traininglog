4a4bb9c0418d4af2ecd82bf9e37e9de8eef649d1
astroritchie <46732388+astroritchie@users.noreply.github.com>
2023-02-03 17:51:43 -0700

    initial workout table + add/del rows

--- R/app_server.R
+++ R/app_server.R
@@ -2,8 +2,54 @@
 #'
 #' @param input,output,session Internal parameters for {shiny}.
 #'     DO NOT REMOVE.
-#' @import shiny
+#' @import shiny DT dplyr
 #' @noRd
+
 app_server <- function(input, output, session) {
-  # Your application server logic
+
+  blankrow <- data.frame(
+    Exercise = character(),
+    Sets = character(),
+    Reps = character(),
+    Load = character(),
+    RPE = character(),
+    Notes = character()
+  )
+
+  rv_table <- reactiveVal(blankrow)
+
+  observeEvent(input$workoutTable_cell_edit, {
+    rv_table(editData(rv_table(), input$workoutTable_cell_edit, 'workoutTable'))
+  })
+
+  observeEvent(input$addButton, {
+    t <- rv_table() %>%
+      add_row(
+        Exercise = '',
+        Sets = '',
+        Reps = '',
+        Load = '',
+        RPE = '',
+        Notes = '')
+
+    rv_table(t)
+  })
+
+  observeEvent(input$delButton, {
+    t <- rv_table()
+
+    if (!is.null(input$workoutTable_rows_selected)) {
+
+      t <- t[-as.numeric(input$workoutTable_rows_selected),]
+    }
+    rv_table(t)
+  })
+
+  output$workoutTable <- DT::renderDataTable({
+    datatable(
+      rv_table(),
+      editable = TRUE,
+      selection = 'multiple')
+  })
+
 }
